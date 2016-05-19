dd32edca26efdc3c57647b4256501a7efa377126
hustcc <i@atool.org>
2016-05-19 17:38:56 +0800

    pos

--- src/sentry/static/sentry/app/views/projectDashboard.jsx
+++ src/sentry/static/sentry/app/views/projectDashboard.jsx
@@ -183,19 +183,6 @@ const ProjectDashboard = React.createClass({
 
         <div className="row">
           <div className="col-md-6">
-            <EventList
-                title={t('Trending Issues')}
-                endpoint={this.getTrendingIssuesEndpoint(dateSince)} />
-          </div>
-          <div className="col-md-6">
-            <EventList
-                title={t('New Issues')}
-                endpoint={this.getNewIssuesEndpoint(dateSince)} />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-6">
             <TopIssueTypePieChart
               endpoint={this.getTopIssueTypesEndpoint(10)}
               cnt="10" />
@@ -204,6 +191,19 @@ const ProjectDashboard = React.createClass({
             <TopIssuePersonPieChart
               endpoint={this.getTopIssuePersonsEndpoint(10)}
               cnt="10" />
+          </div>
+        </div>
+        
+        <div className="row">
+          <div className="col-md-6">
+            <EventList
+                title={t('Trending Issues')}
+                endpoint={this.getTrendingIssuesEndpoint(dateSince)} />
+          </div>
+          <div className="col-md-6">
+            <EventList
+                title={t('New Issues')}
+                endpoint={this.getNewIssuesEndpoint(dateSince)} />
           </div>
         </div>
       </div>

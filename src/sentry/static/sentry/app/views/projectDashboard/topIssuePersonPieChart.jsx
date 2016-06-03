9d6e48602507380d6c5ae95537e2502795cf247a
hustcc <i@atool.org>
2016-06-03 14:59:47 +0800

    update echart

--- src/sentry/static/sentry/app/views/projectDashboard/topIssuePersonPieChart.jsx
+++ src/sentry/static/sentry/app/views/projectDashboard/topIssuePersonPieChart.jsx
@@ -98,6 +98,7 @@ const TopIssuePersonPieChart = React.createClass({
   },
   // Top Person图表加事件
   onChartClicked(params, chart) {
+    console.log(params, params.dataIndex);
     if (params && params.dataIndex) {
       let root_url = '/' + this.props.params.orgId + '/' + this.props.params.projectId + '/?';
       let email = this.state.statsData[params.dataIndex].email;

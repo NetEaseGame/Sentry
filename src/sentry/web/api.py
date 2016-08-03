9bc2ea8497cb24cc85d7d4dc2bba6b4fd76153f7
hustcc <i@atool.org>
2016-08-03 14:07:38 +0800

    update server name

--- src/sentry/web/api.py
+++ src/sentry/web/api.py
@@ -336,7 +336,8 @@ class StoreView(APIView):
         data = helper.validate_data(project, data)
 
         # #845, add for server name filter, by hzwangzhiwei @20160803
-        print 'server_name:', data.get('tags', {})
+        print 'server_name', 
+        print data.get('tags', {})
         server_name = data.get('tags', {}).get('server_name', '')
         if not is_valid_servername(server_name, project):
             raise APIForbidden('Not in Whitelist Server name: %s' % (server_name,))

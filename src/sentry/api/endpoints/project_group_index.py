d5803c37c81172d8c6ce292acb6c3b71ad502d0a
hustcc <i@atool.org>
2016-08-03 15:10:15 +0800

    merge redmine id

--- src/sentry/api/endpoints/project_group_index.py
+++ src/sentry/api/endpoints/project_group_index.py
@@ -535,7 +535,7 @@ class ProjectGroupIndexEndpoint(ProjectEndpoint):
             # primary_group = sorted(group_list, key=lambda x: -x.times_seen)[0]
             # for #847, by hzwangzhiwei @20160803. 
             # when merge trace, primary_group = max(group.redmine_id)
-            primary_group = sorted(group_list, key=lambda x: x.redmine_id and x.redmine_id.isdigit() and int(x.redmine_id) or -1)
+            primary_group = sorted(group_list, key=lambda x: x.redmine_id and x.redmine_id.isdigit() and int(x.redmine_id) or -1)[-1]
             children = []
             for group in group_list:
                 if group == primary_group:

55e0c64f9ac211a132717e2df513a4d76289b065
hustcc <i@atool.org>
2016-08-03 14:08:07 +0800

    update server name

--- src/sentry/web/api.py
+++ src/sentry/web/api.py
@@ -336,7 +336,7 @@ class StoreView(APIView):
         data = helper.validate_data(project, data)
 
         # #845, add for server name filter, by hzwangzhiwei @20160803
-        print 'server_name', 
+        print '=============server_name============='
         print data.get('tags', {})
         server_name = data.get('tags', {}).get('server_name', '')
         if not is_valid_servername(server_name, project):
